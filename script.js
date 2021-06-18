6e4af7699c77a31e9755f4b4f39dbddf7b60481e
piyushsrivastava0004 <50950575+piyushsrivastava0004@users.noreply.github.com>
2021-06-18 18:14:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 var audio = document.createElement('audio');
 var playpauseBtn = document.getElementById('PlayPauseBTN');
 audio.setAttribute('src','sound.mp3');
-audio.loop = true;
-// audio.play();
-var count = 2;
-if(count==2)
-{
-    count = 1;
-    audio.play();
-}
+var count = 0;
 function playpause(){
     if(count == 0){
         count = 1;
